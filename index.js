e251168e44eb89ab87cc28b31f43498026e1d300
riyakwl28 <38571771+riyakwl28@users.noreply.github.com>
2018-08-28 03:44:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+'use-strict'
+const functions = require('firebase-functions');
+const admin=require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.sendNotification=functions.firestore.document("Users/{user_id}/Notifications/{notification_id}").onWrite((data,context)=> {
+	
+	const user_id=context.params.user_id;
+	const notification_id=context.params.notification_id;
+   
+
+    return admin.firestore().collection("Users").doc(user_id).collection("Notifications").doc(notification_id).get().then(queryResult =>{
+   
+    	const from_user_id=queryResult.data().from;
+    	const from_message=queryResult.data().message;
+   
+    	const from_data=admin.firestore().collection("Users").doc(from_user_id).get();
+    	const to_data=admin.firestore().collection("Users").doc(user_id).get();
+
+    	console.log("userid: "+user_id);
+   
+        console.log(admin.firestore().collection("Users").doc(user_id).collection("Notifications"));
+        console.log(admin.firestore().collection("Users"));
+    	return Promise.all([from_data, to_data]).then(result => {
+   
+    		const from_name = result[0].data().name;
+    		const to_name= result[1].data().name;
+    		const token_id= result[1].data().token_id;
+
+    		const payload={
+    			notification: {
+    				title : "Notification From : " + from_name,
+    				body : from_message,
+    				icon : "default",
+    				click_action: "in.notify.firebasepush.TARGETNOTIFICATION"
+    			},
+    			data : {
+    				message: from_message,
+    				from_user_id: from_user_id
+    			}
+    		};
+
+           
+    	 	return admin.messaging().sendToDevice(token_id,payload).then(result =>{
+    	 		console.log("Notification Sent.");
+    	 		return result;
+    	 	});
+    	 
+    });
+});
+});
